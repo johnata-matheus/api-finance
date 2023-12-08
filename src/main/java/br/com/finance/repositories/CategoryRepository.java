ef6ee2b6d6971d9b024a299cc315d6c46a441cba
Johnata Matheus <matheusabdala12@gmail.com>
2023-12-08 00:39:57 -0300

    feat: create CategoryRepository

---
+++ src/main/java/br/com/finance/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package br.com.finance.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.finance.models.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
