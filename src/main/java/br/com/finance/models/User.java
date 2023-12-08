5fe11dd91f8ab9191a86fdb3109c62d062317a81
Johnata Matheus <matheusabdala12@gmail.com>
2023-12-07 22:53:31 -0300

    feat: create User entity

---
+++ src/main/java/br/com/finance/models/User.java
@@ -0,0 +1,31 @@
+package br.com.finance.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String email;
+  
+  @Column(nullable = false)
+  private String password;
+}
