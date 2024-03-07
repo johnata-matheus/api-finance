4dc28e545e53116d86b987400253fd5dbd4940e6
Johnata Matheus <matheusabdala12@gmail.com>
2024-03-07 11:06:40 -0300

    feat: create request validation in RegisterDto

--- src/main/java/br/com/finance/dtos/request/RegisterDto.java
+++ src/main/java/br/com/finance/dtos/request/RegisterDto.java
@@ -2,8 +2,11 @@ package br.com.finance.dtos.request;
 
 import br.com.finance.enums.Role;
 import br.com.finance.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record RegisterDto(String name, String email, String password, Role role){
+public record RegisterDto(@NotBlank @NotNull String name, @Email String email, @NotBlank @NotNull String password, Role role){
   public User toUser(){
     return new User(this.name, this.email, this.password, this.role);
   }
