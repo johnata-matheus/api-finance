58cc7233003a96063e922c2398c80ec2b6802be7
Johnata Matheus <matheusabdala12@gmail.com>
2024-06-09 21:55:51 -0300

    feat: add account controller endpoints

--- src/main/java/br/com/finance/controllers/AccountController.java
+++ src/main/java/br/com/finance/controllers/AccountController.java
@@ -7,8 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,10 +45,48 @@ public class AccountController {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<AccountResponseDto> getAccountById(@PathVariable(value = "id") Long id){
+    var account = this.accountService.findAccountById(id);
+    var accountResponseDto = new AccountResponseDto(account);
+
+    return ResponseEntity.ok().body(accountResponseDto);
+  }
+ 
+
   @PostMapping
   public ResponseEntity<AccountResponseDto> createAccount(@RequestBody @Valid AccountRequestDto request){
-    var account = this.accountService.createAccount(request.toAccount());
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(new AccountResponseDto(account));
+    if(authentication != null && authentication.getPrincipal() instanceof User){
+      User user = (User) authentication.getPrincipal();
+      Long id = user.getId();
+      var account = this.accountService.createAccount(request.toAccount(), id);
+
+      return ResponseEntity.status(HttpStatus.CREATED).body(new AccountResponseDto(account));
+    }
+
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<AccountResponseDto> updateAccount(@PathVariable Long id, @RequestBody AccountRequestDto accountRequestDto){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    User user = (User) authentication.getPrincipal();
+    Long userId = user.getId();
+    
+     try {
+        Account accountUpdate = this.accountService.updateAccount(id, userId, accountRequestDto.toAccount()); 
+        return ResponseEntity.ok().body(new AccountResponseDto(accountUpdate));
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+  }  
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> deleteAccount(@PathVariable(value = "id") Long id){
+    this.accountService.deleteAccount(id);
+
+    return ResponseEntity.noContent().build();
   }
 }
