2ecef7b317cb24f1e9bb4aa859b313735a0156ae
Johnata Matheus <matheusabdala12@gmail.com>
2024-07-05 15:15:48 -0300

    fix: adjust methods in expense controller

--- src/main/java/br/com/finance/controllers/ExpenseController.java
+++ src/main/java/br/com/finance/controllers/ExpenseController.java
@@ -1,5 +1,6 @@
 package br.com.finance.controllers;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.finance.dtos.request.ExpenseRequestDto;
 import br.com.finance.dtos.response.ExpenseResponseDto;
-import br.com.finance.dtos.response.TotalExpenseResponseDto;
-import br.com.finance.dtos.response.ExpensePercentageResponseDto;
+import br.com.finance.dtos.response.PercentageResponseDto;
+import br.com.finance.dtos.response.TotalValueResponseDto;
 import br.com.finance.models.Expense;
 import br.com.finance.models.User;
 import br.com.finance.services.ExpenseService;
@@ -33,30 +34,46 @@ public class ExpenseController {
   @Autowired
   private ExpenseService expenseService;
 
-  @GetMapping("/percentage")
-  public ResponseEntity<ExpensePercentageResponseDto> getExpensePercentage(@RequestParam int yearCurrent, int monthCurrent, int year, int month) {
-    int expense = this.expenseService.getExpensePercentage(yearCurrent, monthCurrent, year, month);
-     
-    return ResponseEntity.ok().body(new ExpensePercentageResponseDto(expense));
+  @GetMapping("/month")
+  public ResponseEntity<List<TotalValueResponseDto>> getTotalExpenses(@RequestParam int year, int month) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if(authentication != null && authentication.getPrincipal() instanceof User){
+      User user = (User) authentication.getPrincipal();
+      Long userId = user.getId();
+      List<Object[]> expenses = this.expenseService.getValueExpenseMonth(userId, year, month);
+      List<TotalValueResponseDto> responseDto = expenses.stream().map(expense -> new TotalValueResponseDto((int) expense[0], (int) expense[1], (int) expense[2], (BigDecimal) expense[3])).toList();
+
+      return ResponseEntity.ok().body(responseDto);
+    }
+
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 
-  @GetMapping("/month")
-  public ResponseEntity<List<TotalExpenseResponseDto>> getTotalExpenses(@RequestParam int year, int month) {
-    List<Object[]> expenses = this.expenseService.getValueExpenseMonth(year, month);
-    List<TotalExpenseResponseDto> responseDto = expenses.stream().map(expense -> new TotalExpenseResponseDto((int) expense[0], (int) expense[1], (int) expense[2], (double) expense[3])).toList();
+  @GetMapping("/percentage")
+  public ResponseEntity<PercentageResponseDto> getExpensePercentage(@RequestParam int yearCurrent, int monthCurrent, int year, int month) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if(authentication != null && authentication.getPrincipal() instanceof User){
+      User user = (User) authentication.getPrincipal();
+      Long userId = user.getId();
+      int expense = this.expenseService.getExpensePercentage(userId, yearCurrent, monthCurrent, year, month);
 
-    return ResponseEntity.ok().body(responseDto);
+      return ResponseEntity.ok().body(new PercentageResponseDto(expense));
+    }
+     
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 
   @GetMapping
   public ResponseEntity<List<ExpenseResponseDto>> getExpensesByUserAuthenticated() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
       
-    if (authentication != null && authentication.getPrincipal() instanceof User) {
+    if (authentication != null && authentication.getPrincipal() instanceof User){
       User user = (User) authentication.getPrincipal();
-      Long idUsuario = user.getId();
-      List<Expense> expenses = this.expenseService.getExpenseByUser(idUsuario);
+      Long userId = user.getId();
+      List<Expense> expenses = this.expenseService.getExpenseByUser(userId);
       List<ExpenseResponseDto> listExpenses = expenses.stream().map(ExpenseResponseDto::new).toList();
+
       return ResponseEntity.ok().body(listExpenses);
     } 
       
@@ -65,9 +82,21 @@ public class ExpenseController {
 
   @GetMapping("/{id}")
   public ResponseEntity<ExpenseResponseDto> getExpenseById(@PathVariable(value = "id") Long id){
-    var expense = this.expenseService.findExpenseById(id);
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    
+    if(authentication != null && authentication.getPrincipal() instanceof User){
+      User user = (User) authentication.getPrincipal();
+      Long userId = user.getId();
+      var expense = this.expenseService.findExpenseById(id);
 
-    return ResponseEntity.ok().body(new ExpenseResponseDto(expense));
+      if(expense != null && expense.getUserId().equals(userId)){
+        return ResponseEntity.ok().body(new ExpenseResponseDto(expense));
+      }
+      
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
  
   @PostMapping
@@ -76,8 +105,8 @@ public class ExpenseController {
     
     if(authentication != null && authentication.getPrincipal() instanceof User){
       User user = (User) authentication.getPrincipal();
-      Long idUser = user.getId();
-      var expense = this.expenseService.createExpense(expenseRequestDto.toExpense(), idUser);
+      Long userId = user.getId();
+      var expense = this.expenseService.createExpense(expenseRequestDto.toExpense(), userId);
 
       return ResponseEntity.status(HttpStatus.CREATED).body(new ExpenseResponseDto(expense));
     }
@@ -86,10 +115,18 @@ public class ExpenseController {
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<ExpenseResponseDto> updateExpense(@PathVariable(value = "id") Long id, @RequestBody @Valid ExpenseRequestDto expenseRequestDto){
-    var expense = this.expenseService.updateExpenseByid(id, expenseRequestDto.toExpense());
+  public ResponseEntity<ExpenseResponseDto> updateExpense(@PathVariable(value = "id") Long idExpense, @RequestBody @Valid ExpenseRequestDto expenseRequestDto){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    
+    if(authentication != null && authentication.getPrincipal() instanceof User){
+      User user = (User) authentication.getPrincipal();
+      Long userId = user.getId();
+      var expense = this.expenseService.updateExpenseById(userId, idExpense, expenseRequestDto.toExpense());
 
-    return ResponseEntity.ok().body(new ExpenseResponseDto(expense));
+      return ResponseEntity.status(HttpStatus.CREATED).body(new ExpenseResponseDto(expense));
+    }
+
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 
   @DeleteMapping("/{id}")
