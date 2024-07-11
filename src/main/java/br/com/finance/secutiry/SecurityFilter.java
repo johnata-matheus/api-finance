863554e2cd86bbb8c443bfbbac12eb674282e926
Johnata Matheus <matheusabdala12@gmail.com>
2024-07-10 22:06:17 -0300

    fix: adjust security filter

--- src/main/java/br/com/finance/secutiry/SecurityFilter.java
+++ src/main/java/br/com/finance/secutiry/SecurityFilter.java
@@ -29,10 +29,14 @@ public class SecurityFilter extends OncePerRequestFilter {
     var token = this.recoverToken(request);
     if(token != null){
       var login = tokenService.validateToken(token);
+
       UserDetails user = userRepository.findByEmail(login);
 
-      var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-      SecurityContextHolder.getContext().setAuthentication(authentication);
+      if(user != null){
+        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+      
     }
     filterChain.doFilter(request, response);
   }
