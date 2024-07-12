f1072c45b982d402d10d67019dd9df590d9cdd7a
Johnata Matheus <matheusabdala12@gmail.com>
2024-07-12 13:35:08 -0300

    fix: remove json property in validate response dto

--- src/main/java/br/com/finance/dtos/response/ValidateReponseDto.java
+++ src/main/java/br/com/finance/dtos/response/ValidateReponseDto.java
@@ -1,7 +1,5 @@
 package br.com.finance.dtos.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record ValidateReponseDto(
-   @JsonProperty("is_valid") boolean isValid
+   boolean is_valid
 ) {}
