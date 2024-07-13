bdc2fbfb103e45d8fb34d6c5f0f8479d86f0c5f3
Johnata Matheus <matheusabdala12@gmail.com>
2024-07-13 17:28:56 -0300

    feat: add query of get accounts of authenticated user

--- src/main/java/br/com/finance/repositories/AccountRepository.java
+++ src/main/java/br/com/finance/repositories/AccountRepository.java
@@ -3,11 +3,14 @@ package br.com.finance.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.finance.models.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+  
+  @Query(value = "SELECT a FROM Account a WHERE userId = :id")
   List<Account> findAccountsByUserId(Long id);
 }
